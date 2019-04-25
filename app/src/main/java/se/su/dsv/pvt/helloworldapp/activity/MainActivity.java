7c3c28c4ce9ad2de4e7da6a34ecabfab22f53666
Niklas Edström <28781038+klnsem@users.noreply.github.com>
2019-04-25 19:34:24 +0200

    Removed code and imports that weren't used, comments added to show what's been done, a file (noter.txt) that kinda explains what's going on here.

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MainActivity.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MainActivity.java
@@ -3,34 +3,25 @@ package se.su.dsv.pvt.helloworldapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import se.su.dsv.pvt.helloworldapp.R;
-import se.su.dsv.pvt.helloworldapp.model.HelloWorldData;
-import se.su.dsv.pvt.helloworldapp.se.su.dsv.pvt.helloworldapp.rest.HelloWorldApiService;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-
-    public static final  String BASE_URL = "https://pvt.dsv.su.se/Group05/";
-
-    private static Retrofit retrofit = null;
-
-    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //connectAndGetApiData();
+        setContentView(R.layout.activity_main); // Här väljs vy-fil! Finns i /res/layout-mappen.
+
     }
+    /**
+     * Den här och den efterföljande metoden startar aktiviteter, och i det här fallet är
+     * aktiviteten en vy. Intent-objektet behövs för att välja vart vi ska: detta genom xxx.class-
+     * ordet. En intent kan innehålla en mängd andra saker, men ja. Sen används metoden startActivity()
+     * för att starta aktiviteten.
+     *
+     */
     public void gotoProfile(View view) {
         Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
@@ -39,43 +30,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Map.class);
         startActivity(intent);
     }
-    /*
-
-    public void connectAndGetApiData() {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
-        }
-
-        HelloWorldApiService helloWorldApiService = retrofit.create(HelloWorldApiService.class);
-        Call<HelloWorldData> call = helloWorldApiService.getJsonResponse();
-
-        call.enqueue(new Callback<HelloWorldData>() {
-            @Override
-            public void onResponse(Call<HelloWorldData> call, Response<HelloWorldData> response) {
-                try {
-                    String resultResponse = response.body().getResult();
-                    String nameResponse = response.body().getName();
-                    String ageResponse = response.body().getAge();
-
-                    resultText = (TextView) findViewById(R.id.responseText);
-                    resultText.setText(resultResponse);
-
-                    Log.d(TAG, "Received data (result, name, age): " + resultResponse + " " + nameResponse + " "
-                            + ageResponse);
-                } catch (NullPointerException e) {
-                    System.out.println("API-data contained null.");
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<HelloWorldData> call, Throwable t) {
-                Log.e(TAG, t.toString());
-            }
-        });
-    }
-    */
-
-
-
 }
--- app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/Profile.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/Profile.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,6 +17,10 @@ import se.su.dsv.pvt.helloworldapp.se.su.dsv.pvt.helloworldapp.rest.HelloWorldAp
 
 public class Profile extends AppCompatActivity {
 
+    /**
+     * de tre första variablerna hör till magin.
+     * De tre efterföljande är de tre TextViews - dvs. textfält - som används i den här vyn.
+     */
     private static final String TAG = Profile.class.getSimpleName();
 
     public static final  String BASE_URL = "https://pvt.dsv.su.se/Group05/";
@@ -36,18 +38,7 @@ public class Profile extends AppCompatActivity {
         connectAndGetApiData();
     }
 
-    /*
-    public void setTextProto(String[] s) {
-        System.out.println("hej");
-        System.out.println(s[1]);
-        final TextView resultView =(TextView)findViewById(R.id.resulttext);
-        final TextView nameView = (TextView) findViewById(R.id.nametext);
-        final TextView ageView = (TextView) findViewById(R.id.agetext);
-        resultView.setText(s[0]);
-        nameView.setText(s[1]);
-        ageView.setText(s[2]);
-    }*/
-
+    // Allting här under är magi, och inget jag kan förklara för varken mig själv eller någon annan.
     public void connectAndGetApiData() {
 
         if (retrofit == null) {
@@ -62,11 +53,15 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onResponse(Call<HelloWorldData> call, Response<HelloWorldData> response) {
                 try {
-                    final String[] output = new String[3];
                     String resultResponse = response.body().getResult();
                     String nameResponse = response.body().getName();
                     String ageResponse = response.body().getAge();
-                    System.out.println(resultResponse + " lol");
+                    /**
+                     * de, uh, sex nedanstående kodraderna deklarerar först de olika TextViews:arna;
+                     * detta sker genom att anropa findViewById, slänga in R.id. och sen namnet
+                     * på komponenten som ska ändras. Detta namn finns i respektive layouts xml-fil,
+                     * vilket i det aktuella fallet är activity_profile.xml.
+                     */
                     resultView = (TextView) findViewById(R.id.resulttext);
                     nameView = (TextView) findViewById(R.id.nametext);
                     ageView = (TextView) findViewById(R.id.agetext);
@@ -88,8 +83,6 @@ public class Profile extends AppCompatActivity {
                 Log.e(TAG, t.toString());
             }
         });
-
-        System.out.println(call.toString());
     }
 
 }
