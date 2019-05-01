2b3a80289f6c672799c3241c9861b676b4b9514a
D. J. Barroso <mr.jockex09@hotmail.com>
2019-05-01 20:11:14 +0200

    La till exempel-markers på kartan + enhetens egna position + hanterar request för permission att ge appen tillåtelse att läsa av positionen

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/Map.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/Map.java
@@ -1,7 +1,15 @@
 package se.su.dsv.pvt.helloworldapp.activity;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +17,14 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import se.su.dsv.pvt.helloworldapp.R;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +36,9 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap = map;
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         CameraPosition cameraPosition = CameraPosition.builder()
                 .target(new LatLng(59.3246656, 18.0410247))
@@ -33,7 +47,55 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
                 .tilt(0)
                 .build();
 
+        googleMap.addMarker((new MarkerOptions()
+                .position(new LatLng(	59.344187, 18.099205))
+                .title("Utegym - Östermalm")
+                .snippet("Tessinparkens norra del nära parkleken.")
+        ));
+
+        googleMap.addMarker((new MarkerOptions()
+                .position(new LatLng(	59.357905, 17.865372))
+                .title("Grimsta utegym")
+                .snippet("Utegymmet är placerat invid Grimsta bollplan.")
+        ));
+
+        if (checkLocationPermission()) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            askPermission();
+        }
+
 //        map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition)); // icke-animerad
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition)); //animerad laddning av kartan
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition)); //animerad laddning av kartan
+    }
+
+    public static final int PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private boolean checkLocationPermission() {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_LOCATION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkLocationPermission())
+                        googleMap.setMyLocationEnabled(true);
+                } else {
+                    Toast.makeText(this, "Required permission was denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+        }
     }
 }
