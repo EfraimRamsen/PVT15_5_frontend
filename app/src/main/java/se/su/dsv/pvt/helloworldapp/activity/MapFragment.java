91a44ac4dc302d9d940f7316af58edf145c9a370
Gosia <gosia.mansen@gmail.com>
2019-05-03 14:04:26 +0200

    Lagt till google maps i mapfragment

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MapFragment.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MapFragment.java
@@ -8,13 +8,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import se.su.dsv.pvt.helloworldapp.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment fragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+
+
     }
 }
