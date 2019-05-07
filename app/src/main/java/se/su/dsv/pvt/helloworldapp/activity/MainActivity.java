92de47d6d32fce23736d55e61b7d6aaac56890ad
Gosia <gosia.mansen@gmail.com>
2019-05-07 19:29:59 +0200

    fixat så att fragment inte återskapas, samt lagt till ett fragment för add challenge

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MainActivity.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MainActivity.java
@@ -1,15 +1,25 @@
 package se.su.dsv.pvt.helloworldapp.activity;
 
 import se.su.dsv.pvt.helloworldapp.fragment.*;
+
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import se.su.dsv.pvt.helloworldapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Fragment fragment1 = new ChallengeFragment();
+    final Fragment fragment2 = new AddChallengeFragment();
+    final Fragment fragment3 = new MapViewFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment1;
 
 
     @Override
@@ -20,33 +30,47 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+        //fm.beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
 
+        //Lägger till fragmenten i fragment_container, och döljer fragment 2 och 3
+        fm.beginTransaction().add(R.id.fragment_container, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.fragment_container, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.fragment_container,fragment1, "1").commit();
 
     }
 
+    //visar det valda fragmnetet och döljer det som va aktivt innan
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-
             switch (item.getItemId()) {
-                case R.id.nav_profile:
-                    selectedFragment = new ProfileFragment();
-                    break;
-                case R.id.nav_map:
-                    selectedFragment = new MapViewFragment();
-                    break;
                 case R.id.nav_challenges:
-                    selectedFragment = new ChallengeFragment();
-                    break;
+                    fm.beginTransaction().hide(active).show(fragment1).commit();
+                    active = fragment1;
+                    return true;
+
+                case R.id.nav_add_challenge:
+                    fm.beginTransaction().hide(active).show(fragment2).commit();
+                    active = fragment2;
+                    return true;
+
+                case R.id.nav_map:
+                    fm.beginTransaction().hide(active).show(fragment3).commit();
+                    active = fragment3;
+                    return true;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-
-            return true;
-
+            return false;
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
