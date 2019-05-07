d5dd33721092f450c62d37a67e5dad8decf90d2f
Niklas Edström <28781038+klnsem@users.noreply.github.com>
2019-05-07 13:24:01 +0200

    removed more code, and some unused imports.

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MainActivity.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/activity/MainActivity.java
@@ -1,45 +1,21 @@
 package se.su.dsv.pvt.helloworldapp.activity;
 
 import se.su.dsv.pvt.helloworldapp.fragment.*;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.widget.Toolbar;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.ImageView;
-
-import com.google.android.gms.maps.MapFragment;
-
 import se.su.dsv.pvt.helloworldapp.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used for the top title bar:
-    public static final int titleIcon = 0;
-    public static final int iconId = R.id.main_icon;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Här väljs vy-fil! Finns i /res/toptoolbar-mappen.
-
-        // Create toolbar:
-        Toolbar toolbar = (Toolbar) TopActionBar
-                .getToolbar(findViewById(TopActionBar.getToolbarIntLink()), true);
-        setSupportActionBar(toolbar);
-        /*
-        int windowWidth = TopActionBar.getWindowWidth(getWindowManager().getDefaultDisplay());
-        toolbar.setTitleMarginStart(windowWidth / 4);
-        toolbar.setTitleMarginEnd(windowWidth / 4);
-        TextView tv = (TextView) toolbar.findViewById(R.id.main_title_text);
-        tv.setText(getString(R.string.main_title));
-        getSupportActionBar().setDisplayShowTitleEnabled(false);*/
-        ImageView iV = (ImageView) toolbar.findViewById(iconId);
-        iV.setImageDrawable(ResourcesCompat.getDrawable(getResources(), titleIcon, null));
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
