37a936636198efa77fcb3403c64b8aefc82baf56
Niklas Edström <28781038+klnsem@users.noreply.github.com>
2019-05-07 13:48:37 +0200

    added empty classes for Place.

---
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/model/OutdoorGym.java
@@ -0,0 +1,10 @@
+package se.su.dsv.pvt.helloworldapp.model;
+
+/**
+ * Denna klass finns för att motsvara vad som finns hos frontenden.
+ * NOTERA: denna klass är tom, då vi inväntar backend-teamet.
+ */
+
+public class OutdoorGym extends Place {
+    ;
+}
---
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/model/Place.java
@@ -0,0 +1,12 @@
+package se.su.dsv.pvt.helloworldapp.model;
+
+/**
+ * Denna klass finns för att motsvara vad som finns hos frontenden, och för att koden ska vara
+ * portabel, dvs. att olika sorters platser är en subklass av denna.
+ * NOTERA: denna klass är tom, då vi inväntar backend-teamet.
+ */
+
+
+public abstract class Place {
+    ;
+}
