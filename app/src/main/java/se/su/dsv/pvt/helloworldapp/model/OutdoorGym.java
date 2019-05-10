6f58eed0c771b7908b35492ad1c5172925198ba3
Niklas Edström <28781038+klnsem@users.noreply.github.com>
2019-05-10 10:48:23 +0200

    added the basic needs of the Place and OutdoorGym-classes.

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/model/OutdoorGym.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/model/OutdoorGym.java
@@ -1,10 +1,17 @@
 package se.su.dsv.pvt.helloworldapp.model;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Denna klass finns för att motsvara vad som finns hos frontenden.
  * NOTERA: denna klass är tom, då vi inväntar backend-teamet.
  */
 
 public class OutdoorGym extends Place {
-    ;
+    private String description;
+
+    public OutdoorGym(LatLng location, String name, int id, String description) {
+        super(location, name, id);
+        this.description = description;
+    }
 }
