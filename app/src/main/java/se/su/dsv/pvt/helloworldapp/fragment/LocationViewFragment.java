29eafe92dbe58f1bfb1c4b4553bca7735ac45834
Niklas Edström <28781038+klnsem@users.noreply.github.com>
2019-05-22 18:59:47 +0200

    Shows a dialog popup when the LocationViewFragment is loaded.

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/LocationViewFragment.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/LocationViewFragment.java
@@ -1,9 +1,11 @@
 package se.su.dsv.pvt.helloworldapp.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,15 @@ public class LocationViewFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         MainActivity mainActivity = (MainActivity) getActivity();
         Place clickedPlace = mainActivity.getOpenThisPlaceFragment();
+        /**
+         * this is for trying out/debugging the popup dialog!
+         * TODO: make something nicer of it.
+         */
+        AlertDialog.Builder adb = new AlertDialog.Builder(getContext());
+        adb.setView(R.layout.rank_gym_dialog);
+        adb.create();
+        adb.show();
+
         //System.err.println(clickedPlace.getName()); // tested if we got a Place.
         return inflater.inflate(R.layout.fragment_location_view, container, false);
     }
