276be0a17ecb49c96c56c22790ea468c71ec71e8
Niklas Edström <28781038+klnsem@users.noreply.github.com>
2019-05-23 14:53:10 +0200

    All the buttons and whatnot works, as in i know how to use them for what they're supposed to be doing.

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/LocationViewFragment.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/LocationViewFragment.java
@@ -1,8 +1,5 @@
 package se.su.dsv.pvt.helloworldapp.fragment;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +8,8 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import se.su.dsv.pvt.helloworldapp.R;
 import se.su.dsv.pvt.helloworldapp.activity.MainActivity;
@@ -18,35 +17,77 @@ import se.su.dsv.pvt.helloworldapp.model.Place;
 
 public class LocationViewFragment extends Fragment {
 
+    protected AlertDialog alertDialog;
+    private int clickedStar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         MainActivity mainActivity = (MainActivity) getActivity();
         Place clickedPlace = mainActivity.getOpenThisPlaceFragment();
+        //System.err.println(clickedPlace.getName()); // tested if we got a Place.
         /**
          * this is for trying out/debugging the popup dialog!
          * TODO: make something nicer of it.
-
-        AlertDialog.Builder adb = new AlertDialog.Builder(getContext());
-        adb.setView(R.layout.rank_gym_dialog);
-        adb.create();
-        adb.show();
          */
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(R.layout.rank_gym_dialog);
-        // builder.setItems();
-        AlertDialog alertDialog = builder.create();
+        alertDialog = builder.create();
+        alertDialog.show();
+        /**
+         * Creates listeners for the buttons and stars
+         * @author Niklas Edström
+         */
+        Button cancelButton = (Button) alertDialog.findViewById(R.id.rankgym_button_cancel);
+        cancelButton.setOnClickListener(new cancelButtonListener());
+        Button confirmButton = (Button) alertDialog.findViewById(R.id.rankgym_button_ok);
+        confirmButton.setOnClickListener(new confirmButtonListener());
 
-        //System.err.println(clickedPlace.getName()); // tested if we got a Place.
+        starButtonListener starbl = new starButtonListener();
+
+        ImageView ib1 = (ImageView) alertDialog.findViewById(R.id.rankgym_imageButton1);
+        ImageView ib2 = (ImageView) alertDialog.findViewById(R.id.rankgym_imageButton2);
+        ImageView ib3 = (ImageView) alertDialog.findViewById(R.id.rankgym_imageButton3);
+        ImageView ib4 = (ImageView) alertDialog.findViewById(R.id.rankgym_imageButton4);
+        ImageView ib5 = (ImageView) alertDialog.findViewById(R.id.rankgym_imageButton5);
+
+        ib1.setOnClickListener(starbl);
+        ib2.setOnClickListener(starbl);
+        ib3.setOnClickListener(starbl);
+        ib4.setOnClickListener(starbl);
+        ib5.setOnClickListener(starbl);
+
+
         return inflater.inflate(R.layout.fragment_location_view, container, false);
     }
 
-    class cancelButtonListener implements DialogInterface.OnClickListener {
+    /**
+     * These listeners are used by the popup window.
+     * @author Niklas Edström
+     */
+    class cancelButtonListener implements View.OnClickListener {
         @Override
-        public void onClick(DialogInterface dialog, int which) {
+        public void onClick(View v) {
+            alertDialog.cancel();
+        }
+    }
+    class confirmButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            /**
+             * a) Läs av clickedStar,
+             * b) Addera användarens betyg (clickedStar) till databasen.
+             */
             ;
-
+        }
+    }
+    public class starButtonListener implements ImageView.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            System.out.println(id);
+            ImageView iV = v.findViewById(id);
+            iV.setImageResource(R.drawable.ic_back_button);
         }
     }
 }
--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/RankGymDialog.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/RankGymDialog.java
@@ -3,22 +3,46 @@ package se.su.dsv.pvt.helloworldapp.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import se.su.dsv.pvt.helloworldapp.R;
+import se.su.dsv.pvt.helloworldapp.activity.MainActivity;
 
 public class RankGymDialog extends Activity {
 
     View view = null;
+    ViewGroup viewGroup = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rank_gym_dialog);
+        System.out.println("den creatar ivf");
+
         LayoutInflater inflater = getLayoutInflater();
         Context c = inflater.getContext();
-        view = inflater.inflate()
+        LayoutInflater.Factory lif = inflater.getFactory();
+        view = lif.onCreateView("rankgymdialogxml", c, findViewById(R.id.rankgymdialogxml));
+        Button cancelButton = view.findViewById(R.id.rankgym_button_cancel);
+        cancelButton.setOnClickListener(new cancelButtonListener());
+        setContentView(view);
 
+
+    }
+
+    public View getView() {
+        return this.view;
+    }
+
+    class cancelButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            System.out.println("it must be bunnies");
+        }
     }
 }
