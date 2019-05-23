2013f146bbafbbd1395753b2da111e147bfcbddd
Paula Silveira <pasi2645@student.su.se>
2019-05-23 14:51:57 +0200

    remove unnecessary imports

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/AddChallengeFragment.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/AddChallengeFragment.java
@@ -4,23 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import android.view.*;
+import android.widget.*;
+import java.util.*;
 
 import se.su.dsv.pvt.helloworldapp.R;
 import se.su.dsv.pvt.helloworldapp.activity.MainActivity;
