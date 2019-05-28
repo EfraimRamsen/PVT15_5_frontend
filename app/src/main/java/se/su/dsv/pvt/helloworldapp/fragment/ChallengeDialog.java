07ae1eb3dfa7b9ee407227d89f3cd11b30069c4b
Paula Silveira <pasi2645@student.su.se>
2019-05-28 13:59:49 +0200

    merge before refactoring

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/ChallengeDialog.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/ChallengeDialog.java
@@ -10,29 +10,20 @@ import android.widget.Button;
 import se.su.dsv.pvt.helloworldapp.R;
 import se.su.dsv.pvt.helloworldapp.activity.MainActivity;
 
-public class MyCustomDialog extends DialogFragment {
+public class ChallengeDialog extends DialogFragment {
 
-    private static final String TAG = "MyCustomDialog";
+    private static final String TAG = "ChallengeDialog";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
         MainActivity mainActivity = (MainActivity) getActivity();
-        View view = inflater.inflate(R.layout.dialog_cancel, container, false);
-        Button yes = view.findViewById(R.id.yes);
-        Button no = view.findViewById(R.id.no);
+        View view = inflater.inflate(R.layout.dialog_challenge, container, false);
+        Button ok = view.findViewById(R.id.ok);
 
-        no.setOnClickListener(new View.OnClickListener(){
+        ok.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                getDialog().dismiss();
-            }
-        });
-
-        yes.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                mainActivity.clearAddChallenge();
                 getDialog().dismiss();
             }
         });
--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/CustomAdapter.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/CustomAdapter.java
@@ -4,24 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
 import se.su.dsv.pvt.helloworldapp.R;
 import se.su.dsv.pvt.helloworldapp.model.Challenge;
 
 
-public class CustomAdapter extends ArrayAdapter<Challenge> implements View.OnClickListener {
+public class CustomAdapter extends ArrayAdapter<Challenge> {
 
     private ArrayList<Challenge> challenges;
     Context mContext;
-
-    @Override
-    public void onClick(View v) {
-        
-    }
 
     private static class ViewHolder {
         TextView name;
@@ -38,28 +34,30 @@ public class CustomAdapter extends ArrayAdapter<Challenge> implements View.OnCli
     private int lastPosition = -1;
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View vy, ViewGroup parent) {
         // Get the data item for this position
         Challenge challenge = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         ViewHolder viewHolder; // view lookup cache stored in tag
+        ImageButton more = vy.findViewById(R.id.more);
+        more.setOnClickListener((View.OnClickListener) this);
 
         final View result;
 
-        if (convertView == null) {
+        if (vy == null) {
 
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.list_item, parent, false);
-            viewHolder.name = convertView.findViewById(R.id.challenge_name);
-            viewHolder.description = convertView.findViewById(R.id.description);
+            vy = inflater.inflate(R.layout.list_item, parent, false);
+            viewHolder.name = vy.findViewById(R.id.challenge_name);
+            viewHolder.description = vy.findViewById(R.id.description);
 
-            result=convertView;
+            result=vy;
 
-            convertView.setTag(viewHolder);
+            vy.setTag(viewHolder);
         } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-            result=convertView;
+            viewHolder = (ViewHolder) vy.getTag();
+            result=vy;
         }
 
         lastPosition = position;
@@ -68,6 +66,11 @@ public class CustomAdapter extends ArrayAdapter<Challenge> implements View.OnCli
         viewHolder.description.setText(challenge.getDescription());
 
         // Return the completed view to render on screen
-        return convertView;
+        return vy;
     }
+
+    public void onClick(View v){
+
+    }
+
 }
