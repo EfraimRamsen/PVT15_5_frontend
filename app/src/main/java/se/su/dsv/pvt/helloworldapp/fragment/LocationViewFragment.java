347b6ad6da538dd0b3b7836bcb995bb755c1d00d
D. J. Barroso <mr.jockex09@hotmail.com>
2019-06-01 00:28:57 +0200

    Made challenge info visible on the dialog in LocationView

--- app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/LocationViewFragment.java
+++ app/src/main/java/se/su/dsv/pvt/helloworldapp/fragment/LocationViewFragment.java
@@ -49,6 +49,11 @@ public class LocationViewFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ChallengeDialog cd = new ChallengeDialog();
+                //RADEN UNDER HÄMTAR DET OBJEKT SOM ANVÄNDAREN KLICKAT PÅ I LISTAN
+                Challenge c = adapter.getItem(position);
+                //RADEN UNDER ANROPAR EN METOD I CHALLENGEDIALOG OCH SKICKAR MED OBJEKTET SOM ANVÄNDAREN VALT
+                //METODEN FINNS PÅ RAD 103 I CHALLENGEDIALOG
+                cd.updateView(c);
                 cd.show(getFragmentManager(), "ChallengeDialog");
                 Log.d(MainActivity.class.getSimpleName(), "hej");
             }
